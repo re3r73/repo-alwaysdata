15fed17b7c37a9ac2822ca248c0125c4284639c0
Jeremie Poujol <jeremiepoujol@gmail.com>
2025-06-13 13:45:19 +0200

    Update evaluation.php

--- HYDRO/evaluation.php
+++ HYDRO/evaluation.php
@@ -1,7 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-// evaluation.php - Contenu HTML fourni via interface (remplacé ici par HTML intégré)
-include("evaluation_content.html");
-?> 
+$eleveId = $_GET['eleve'] ?? null;
+$dataFile = "data/fiche_{$eleveId}.json";
+header("Content-Type: application/json");
+if (file_exists($dataFile)) {
+  echo file_get_contents($dataFile);
+} else {
+  echo json_encode(["message" => "Aucune donnée pour cet élève"]);
+}
+?>
