38b24ed2ea49334a9a34dee7dc9e4103c4c869db
Jeremie Poujol <jeremiepoujol@gmail.com>
2025-06-13 11:38:05 +0200

    Update evaluation.php

--- HYDRO/evaluation.php
+++ HYDRO/evaluation.php
@@ -4,4 +4,4 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // evaluation.php - Contenu HTML fourni via interface (remplacé ici par HTML intégré)
 include("evaluation_content.html");
-?>
+?> 
