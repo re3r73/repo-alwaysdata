5cf5c34f5f111d8357f0f2b7fa176aade38c80d3
Jeremie Poujol <jeremiepoujol@gmail.com>
2025-06-13 11:37:01 +0200

    Add files via upload

--- HYDRO/evaluation.php
+++ HYDRO/evaluation.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // evaluation.php - Contenu HTML fourni via interface (remplacé ici par HTML intégré)
 include("evaluation_content.html");
 ?>
